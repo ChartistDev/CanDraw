1ce2962ab4812b4b2c4ade62a9742317861e3316
ChartistDev <50948001+ChartistDev@users.noreply.github.com>
2019-07-14 16:34:24 +0530

    Add files via upload

---
+++ src/canviz/canvizconstructor.js
@@ -0,0 +1,11 @@
+class CanViz {
+    constructor(){
+        this.settings = {};
+        return(this);
+    }
+    drawShape = (_shapeObject) => {
+        var shapeObject = _shapeObject;
+        console.log(shapeObject);
+    }
+}
+export default CanViz;
