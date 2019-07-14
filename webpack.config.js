d906a0623397cb087e12ccb6bb003a00d648576b
Debanjan Dey <debanjandey@FCs-MacBook-Pro.local>
2019-07-14 16:47:15 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: './dest/bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devServer: {
+        port: 3000
+    }
+};
